ae79e5ae294e85d714350d224732df0200ec4e2d
Oli <oliver.berry84@gmail.com>
2018-03-21 11:46:30 +0000

    help

---
+++ app/src/main/java/com/example/cerberus/rockpaperscissors/Game.java
@@ -0,0 +1,14 @@
+package com.example.cerberus.rockpaperscissors;
+
+/**
+ * Created by cerberus on 21/03/2018.
+ */
+
+public class Game {
+
+    //To do.. getMove for player
+    public static ResultType play(){
+
+    }
+}
+
---
+++ app/src/main/java/com/example/cerberus/rockpaperscissors/ResultType.java
@@ -0,0 +1,11 @@
+package com.example.cerberus.rockpaperscissors;
+
+/**
+ * Created by cerberus on 21/03/2018.
+ */
+
+public enum ResultType {
+    DRAW,
+    COMPUTERWINS,
+    YOUWIN
+}
