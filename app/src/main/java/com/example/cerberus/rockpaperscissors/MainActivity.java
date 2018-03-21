550412c96db23d21f2f5b7e53cbb1735a70173e7
drp33 <dan@mechanochemistry.org>
2018-03-21 13:31:16 +0000

    update gitignore

--- app/src/main/java/com/example/cerberus/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/cerberus/rockpaperscissors/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.cerberus.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button rockButton, paperButton, scissorsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate");
+        rockButton = findViewById(R.id.rockButton);
+        paperButton = findViewById(R.id.paperButton);
+        scissorsButton = findViewById(R.id.scissorsButton);
+    }
+
+    public void onClickButton(View clickedView) {
+        Log.d("MainActivity", "onClickButton");
+        clickedView.getId();
     }
 }
